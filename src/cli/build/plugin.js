39874d30bcb84c8a92bdcd3c01dd496e731fa49f
Christian <schroeder@mailbox.org>
2023-05-19 13:35:57 +0200

    include nesting

--- src/cli/build/plugin.js
+++ src/cli/build/plugin.js
@@ -8,7 +8,7 @@ import loadPlugins from 'postcss-load-config/src/plugins' // Little bit scary, l
 import loadOptions from 'postcss-load-config/src/options' // Little bit scary, looking at private/internal API
 
 import tailwind from '../../processTailwindFeatures'
-import { loadAutoprefixer, loadCssNano, loadPostcss, loadPostcssImport } from './deps'
+import { loadAutoprefixer, loadCssNano, loadPostcss, loadPostcssImport, loadPostcssNested } from './deps'
 import { formatNodes, drainStdin, outputFile } from './utils'
 import { env } from '../../lib/sharedState'
 import resolveConfig from '../../../resolveConfig.js'
@@ -315,6 +315,7 @@ export async function createProcessor(args, cliConfigPath) {
     ...afterPlugins,
     !args['--no-autoprefixer'] && loadAutoprefixer(),
     args['--minify'] && loadCssNano(),
+    !args['--no-nesting'] && loadPostcssNested(),
   ].filter(Boolean)
 
   /** @type {import('postcss').Processor} */
